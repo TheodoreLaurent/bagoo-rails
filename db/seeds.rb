12691c65872968cf31eb3cf2a362cc9b2e2b69b5
Remi dYvoire <remi.dyvoire@gmail.com>
2017-02-20 17:06:57 +0000

    adding 2 bags in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+bags = Bag.create([{ category: 'travel', brand: 'Vuitton', capacity: '30', price_per_day: '30', description: 'great bag' }, { category: 'Luxury', brand: 'Gucci', capacity: '10', price_per_day: '100', description: 'great luxury bag' }])
