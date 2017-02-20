739d2af2c33fdb2dce0247cf61bf9bb910b959c1
Evan Thomas <evanhen@hotmail.co.uk>
2017-02-20 18:22:02 +0000

    added pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220152048) do
+ActiveRecord::Schema.define(version: 20170220173752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,11 +23,22 @@ ActiveRecord::Schema.define(version: 20170220152048) do
     t.integer  "capacity"
     t.float    "price_per_day"
     t.text     "description"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_bags_on_user_id", using: :btree
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.date     "check_in"
+    t.date     "check_out"
+    t.float    "total_price"
+    t.float    "rating"
+    t.text     "content"
+    t.integer  "bag_id"
+    t.integer  "user_id"
+    t.index ["bag_id"], name: "index_bookings_on_bag_id", using: :btree
+    t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -47,4 +58,7 @@ ActiveRecord::Schema.define(version: 20170220152048) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bags", "users"
+  add_foreign_key "bookings", "bags"
+  add_foreign_key "bookings", "users"
 end
