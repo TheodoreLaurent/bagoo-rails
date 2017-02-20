82ce4468865033b6764d5137e514ca1173303baa
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-02-20 16:52:46 +0000

    conflict resolved

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,6 @@ ActiveRecord::Schema.define(version: 20170220152048) do
   enable_extension "plpgsql"
 
   create_table "bags", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "category"
