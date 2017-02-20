4b5a75834d6e00db4cb87bbaca4bb8c94b3d9ddc
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-02-20 17:56:22 +0000

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220152048) do
+ActiveRecord::Schema.define(version: 20170220170313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,13 @@ ActiveRecord::Schema.define(version: 20170220152048) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.date     "check_in"
+    t.date     "check_out"
+    t.float    "total_price"
+    t.float    "rating"
+    t.text     "content"
   end
 
   create_table "users", force: :cascade do |t|
