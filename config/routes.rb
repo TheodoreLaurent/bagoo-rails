141907b882c08fb7cc0187db393ccd907049ab68
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-02-21 16:41:08 +0000

    Added Bascil Profile Page

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   resources :bookings, only: [:show]
 
-
+  get '/profile' => 'pages#profile'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
