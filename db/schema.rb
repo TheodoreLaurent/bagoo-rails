4e8845f9aa03b4f49507f40d83d7b91d77ce5d70
Evan Thomas <evanhen@hotmail.co.uk>
2017-02-21 11:35:04 +0000

    User can add a bag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220173752) do
+ActiveRecord::Schema.define(version: 20170220182313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170220173752) do
     t.float    "price_per_day"
     t.text     "description"
     t.integer  "user_id"
+    t.string   "title"
     t.index ["user_id"], name: "index_bags_on_user_id", using: :btree
   end
 
