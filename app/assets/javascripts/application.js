387ffa0e64e004a0ed88174c652155a6dd3804b5
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-02-21 11:49:02 +0000

    Search Bar working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,27 +4,30 @@
 //= require_tree .
 
 
-$(window).ready(function(){
-      var nowTemp = new Date();
-      var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
-      var checkin = $('#checkin').datepicker({
-        onRender: function(date) {
-          return date.valueOf() < now.valueOf() ? 'disabled' : '';
-        }
-      }).on('changeDate', function(ev) {
-        if (ev.date.valueOf() > checkout.date.valueOf()) {
-          var newDate = new Date(ev.date)
-          newDate.setDate(newDate.getDate() + 1);
-          checkout.setValue(newDate);
-        }
-        checkin.hide();
-        $('#checkout')[0].focus();
-      }).data('datepicker');
-      var checkout = $('#checkout').datepicker({
-        onRender: function(date) {
-          return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
-        }
-      }).on('changeDate', function(ev) {
-        checkout.hide();
-      }).data('datepicker');
-    });
+$(document).ready(function(){
+  $('.button').css( 'cursor', 'pointer' );
+  $('.form-control').css( 'cursor', 'pointer' );
+  var nowTemp = new Date();
+  var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+  var checkin = $('#checkin').datepicker({
+    onRender: function(date) {
+      return date.valueOf() < now.valueOf() ? 'enabled' : '';
+    }
+  }).on('changeDate', function(ev) {
+    if (ev.date.valueOf() > checkout.date.valueOf()) {
+      var newDate = new Date(ev.date)
+      newDate.setDate(newDate.getDate() + 1);
+      checkout.setValue(newDate);
+    }
+    checkin.hide();
+    $('#checkout')[0].focus();
+  }).data('datepicker');
+  var checkout = $('#checkout').datepicker({
+    onRender: function(date) {
+      return date.valueOf() <= checkin.date.valueOf() ? 'enabled' : '';
+    }
+  }).on('changeDate', function(ev) {
+    checkout.hide();
+  }).data('datepicker');
+
+});
