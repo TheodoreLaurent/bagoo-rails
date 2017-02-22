c5886c4f0a05f87d00f751532cb6d88bb7fb175a
Evan Thomas <evanhen@hotmail.co.uk>
2017-02-22 16:43:10 +0000

    got the inbox and message show page working...

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,9 +11,21 @@ class PagesController < ApplicationController
   def bookings
     @user_id = current_user.id
     @bookings = Booking.where(user_id: @user_id)
+  end
 
+  def inbox
+    @user = current_user
+    @conversations = @user.mailbox.inbox
+  end
+
+  def message_show
+    @user = current_user
+    @conversations = @user.mailbox.inbox
+    @conversations = @conversations.find(params[:id])
   end
 
 end
 
 
+# @conversations = @user.mailbox.inbox.first
+#     @receipts = @conversations.receipts_for @user
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
   get '/profile' => 'pages#profile'
   get '/listings' => 'pages#listings'
   get '/my-bookings' => 'pages#bookings'
+  get '/inbox' => 'pages#inbox'
+  get '/message' => 'pages#message'
+  get '/message/:id' => 'pages#message_show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
