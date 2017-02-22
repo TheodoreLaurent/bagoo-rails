82ea9870c68ec349dec4e3ed9ba0b71d6a1005c0
Joy Joseph Abisaab <joy.abisaab@gmail.com>
2017-02-22 11:52:37 +0000

    main button and cards work

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, only: [:show]
-  
+
   get '/profile' => 'pages#profile'
+  get '/listings' => 'pages#listings'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
